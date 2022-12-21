5ba85d10558d920a3a3b1678c8e8e9d508610fea
Brian Fontillas <BrianKFontillas@gmail.com>
2022-12-21 12:53:08 -0800

    adding class skeletons

---
+++ hangman_v2.js
@@ -0,0 +1,27 @@
+const rl = require("readline-sync");
+const rw = require("random-words");
+const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split("");
+
+class Game {
+	constructor() {
+		
+	}
+
+	static welcome() {
+		console.log("Welcome to Hangman!");
+		rl.question("Press the enter key to continue...");
+	}
+}
+
+class Player {
+	constructor() {
+
+	}
+}
+
+class Word {
+	constructor() {
+
+	}
+}
+
